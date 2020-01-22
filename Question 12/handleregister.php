bfc369182b617d9c1d9334cb7c746a7257d6758f
Mohamed Bakr <m.hishambakr@gmail.com>
2020-01-22 16:25:53 +0200

    second commit

--- Question 12/handleregister.php
+++ Question 12/handleregister.php
@@ -23,14 +23,18 @@ $id = $_POST["id"];
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-$query = "INSERT INTO users
-(id, email, password)
-VALUES('$id','$username','$password')";
+if (is_numeric($id) == false) {
+    echo "'$id' is not a number";
+} elseif(strlen($username) > 25){
+    echo "length of username should be 25 character or less";
+} elseif (strlen($password) > 15){
+    echo "password length should be 15 character or less";
+} else{
+    $query = "INSERT INTO users
+    (id, email, password)
+    VALUES('$id','$username','$password')";
 
+    $result=$conn->QUERY($query); 
+}
 
-$result=$conn->QUERY($query); 
-
-
-
- 
 ?>
--- Question 12/register.php
+++ Question 12/register.php
@@ -1,5 +1,5 @@
 <form action="handleregister.php" method="POST">
-    <input name="id" type="number" placeholder="id">
+    <input name="id" type="text" placeholder="id">
     <input name="username" type="text" placeholder="Enter username: ">
     <input name="password" type="password" placeholder="Enter password: ">
     <input type="submit" value="register">
